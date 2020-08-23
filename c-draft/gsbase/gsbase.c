26a5382f5ea8d36676cb41af104a5f32d51dbe63
pingjui <pingjuiliao@gmail.com>
2020-08-22 23:51:24 -0700

    prologue 100%

--- c-draft/gsbase/gsbase.c
+++ c-draft/gsbase/gsbase.c
@@ -6,26 +6,63 @@
 #include <asm/prctl.h>
 #include <errno.h>
 #include <sys/prctl.h>
-int
-main(void) {
+void
+gsbase_libc(void) {
     unsigned long *in, *out ; 
     int r = 0 ;
+
+    puts("In gsbase_libc") ;
+    
     arch_prctl(ARCH_GET_GS, out);
-    printf("gsbase : %p\n", out);
+    printf("\toriginal gsbase : %p\n", out);
     out = NULL ; 
     
-    puts("start setting !!!") ;
     in = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0) ;
     if ( (r = arch_prctl(ARCH_SET_GS, (unsigned long) in)) != 0 ) {
-        printf("setting failed, errno : %s\n", strerror(errno));
+        printf("\tsetting failed, errno : %s\n", strerror(errno));
         exit(-1) ;
     } 
     if ( (r = arch_prctl(ARCH_GET_GS, &out)) != 0 ) {
-        printf("getting failed, errno : %s\n", strerror(errno)) ;
+        printf("\tgetting failed, errno : %s\n", strerror(errno)) ;
         exit(-1) ;
     }
-    printf("gsbase : %p\n", out);
-    printf("mmap   : %p\n", in); 
+    printf("\tmmap   : %p\n", in); 
+    printf("\tgsbase : %p\n", out);
  
-    return  0  ;
+    return; 
 }
+
+void
+gsbase_syscall(void) {
+    unsigned long *in = 0, *out= 0;
+    puts("\nin gsbase_syscall") ;
+    in = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+    asm volatile(
+            "movq $158,    %%rax\n\t"
+            //"movq $0x0,    %%rdi\n\t"
+            "movq $4097, %%rdi\n\t"
+            "movq %0  ,    %%rsi\n\t"
+            "syscall\n\t" 
+            "movq $158,    %%rax\n\t"
+            //"movq $0x0,    %%rdi\n\t"
+            "movq $4100, %%rdi\n\t"
+            "leaq %1  ,    %%rsi\n\t"
+            "syscall\n\t"
+            :
+            : "m"(in), "m"(out)
+            :
+            );
+    printf("\tmmap  : %p\n", in);
+    printf("\tgsbase: %p\n", out);
+    return ;
+}
+
+int
+main(void) {
+    gsbase_libc() ;
+    gsbase_syscall();
+    return 0;
+}
+
+
+
--- llvm/lib/Target/X86/X86ShadowStackPass.cpp
+++ llvm/lib/Target/X86/X86ShadowStackPass.cpp
@@ -1,5 +1,6 @@
 #include "X86.h"
 #include "X86InstrInfo.h"
+#include "X86InstrBuilder.h"
 #include "X86Subtarget.h"
 #include "X86RegisterInfo.h"
 #include "X86TargetMachine.h"
@@ -50,7 +51,8 @@ namespace {
             DebugLoc DL = FirstMBB.findDebugLoc(MBBI) ;
             const X86InstrInfo *X86II = STI.getInstrInfo() ;
             
-            // mmap(NULL, 0x1000, )
+            // void *gsbase = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, 
+            //                      MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) ;
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri), X86::RDI)
                 .addImm(0x0) ;
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::ESI)
@@ -63,9 +65,50 @@ namespace {
                 .addImm(0xffffffff);
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::R9D)
                 .addImm(0x0);
-            // const MCSymbol *TlsGetAddr = Ctx
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::CALL64pcrel32))
                 .addExternalSymbol("mmap");
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64rr), X86::RBX)
+                .addReg(X86::RAX) ;
+
+            // arch_prctl(ARCH_SET_GS, gsbase);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::RDI)
+                .addImm(0x1001);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64rr), X86::RSI)
+                .addReg(X86::RBX);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::CALL64pcrel32))
+                .addExternalSymbol("arch_prctl");
+
+            // void *shadow_stack_top = mmap(NULL, 0x1000, ... ) 
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri), X86::RDI)
+                .addImm(0x0) ;
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::ESI)
+                .addImm(0x1000);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::EDX)
+                .addImm(0x3);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::ECX)
+                .addImm(0x22);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::R8D)
+                .addImm(0xffffffff);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64ri32), X86::R9D)
+                .addImm(0x0);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::CALL64pcrel32))
+                .addExternalSymbol("mmap");
+            
+            // *gsbase = shadow_stack_top ;
+            // mov %shadow_stack_top, (%gsbase) 
+            // ATT: mov %rax, 0x0(%rbx)
+            addRegOffset(BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64mr))
+                    , X86::RBX, false, 0x0)
+                .addReg(X86::RAX);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::XOR64rr), X86::RBX)
+                .addReg(X86::RBX, RegState::Undef)
+                .addReg(X86::RBX, RegState::Undef);
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::XOR64rr), X86::RAX)
+                .addReg(X86::RAX, RegState::Undef)
+                .addReg(X86::RAX, RegState::Undef);
+            
+            // NOP for debug
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::NOOP));
         }
 
         void createShadowStackPrologue(MachineFunction &MF) {
@@ -77,7 +120,41 @@ namespace {
             MachineBasicBlock::iterator MBBI = FirstMBB.begin() ;
             DebugLoc DL = FirstMBB.findDebugLoc(MBBI) ;
             const X86InstrInfo *X86II = STI.getInstrInfo() ;
+            
+            // mov %gs:0x0, %rax
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64rm), X86::RAX)
+               .addReg(0x0)
+               .addImm(0x1)
+               .addReg(0x0)
+               .addImm(0x0)
+               .addReg(X86::GS);   // base, scale, Index, Disp, Segment 
+            // 1) 
+            // mov 0x8(%rbp), %rbx
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64rm), X86::RBX)
+                .addReg(X86::RBP)
+                .addImm(0x1)
+                .addReg(0x0)
+                .addImm(0x8)
+                .addReg(0x0);
 
+            // mov %rbx, 0x0(%rax)
+            addRegOffset(BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64mr))
+                    , X86::RAX, false, 0x0)
+                .addReg(X86::RBX);
+            
+            // add %rax, 8
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::ADD64ri32), X86::RAX)
+                .addReg(X86::RAX)
+                .addImm(8) ;
+            
+            BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::MOV64mr))
+                .addReg(0x0)
+                .addImm(0x1)
+                .addReg(0x0)
+                .addImm(0x0)
+                .addReg(X86::GS)
+                .addReg(X86::RAX);
+            // NOP for debug 
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::NOOP)) ;
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::NOOP)) ;
             BuildMI(FirstMBB, MBBI, DL, X86II->get(X86::NOOP)) ;
