c8276b9d544441657265d838a75f6dd66ad64cf8
pingjui <pingjuiliao@gmail.com>
2021-10-12 16:46:58 -0700

    change to llvm monoproject

--- compile.sh
+++ compile.sh
@@ -3,7 +3,10 @@ SOURCE_FILE=main.c
 IR_FILE=main.bc
 OBJ_FILE=main.o
 BIN_FILE=main.exe
-## assume
-/usr/bin/clang -emit-llvm -c ${SOURCE_FILE}
-./llvm/build/bin/llc --filetype=obj ${IR_FILE}
-/usr/bin/clang ${OBJ_FILE} -o  ${BIN_FILE}
+MY_CLANG=./llvm-project-10.0.1/build/bin/clang
+## assume clang exists
+#/usr/bin/clang -emit-llvm -c ${SOURCE_FILE}
+#./llvm/build/bin/llc --filetype=obj ${IR_FILE}
+#/usr/bin/clang ${OBJ_FILE} -o  ${BIN_FILE}
+
+${MY_CLANG} ${SOUCE_FILE} -o ${BIN_FILE}
--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ def error(msg) :
 
 def build_llvm() :
 
-    llvm_root = os.path.abspath("./llvm")
+
+
+    llvm_root = os.path.abspath("./llvm-project-10.0.1")
     llvm_build= llvm_root + "/build"
     clang = llvm_build + "/bin/clang"
     if os.path.exists(clang) :
@@ -24,14 +26,27 @@ def build_llvm() :
 
     # cmake
     if len(ls) == 0 :
-        os.system('cmake ..')
+        os.system('cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm')
 
     # make
     os.system('make')
     os.chdir(pwd)
 
+def build_pass() :
+    build_dir = os.path.abspath("./build")
+    if not os.path.exists(build_dir) :
+        os.mkdir(build_dir)
+
+    os.chdir(build_dir)
+    # cmake
+    os.system('cmake ..')
+
+    # make
+    os.system('make')
+
 def main() :
     build_llvm()
+    # build_pass()
     print("Done !")
 
 if __name__ == '__main__' :
